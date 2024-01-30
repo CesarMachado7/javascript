1aceee7cfaf028f3f0f2ffca11044f4813c22b39
Cesar Machado <machadocesar566@gmail.com>
2024-01-30 19:58:28 -0300

    Update ex013.js

--- aula012/ex013.js
+++ aula012/ex013.js
@@ -2,21 +2,27 @@ var agora = new Date()
 var Diasem = agora.getDay()
 
 switch(Diasem){
-    case 0:
+
+        case 0:
         console.log('Domingo')
         break
+
         case 1:
         console.log('Segunda')
         break
+
         case 2:
         console.log('Terca')
         break
+        
         case 3:
         console.log('Quarta')
         break
+
         default:
-            console.log('Erro')
-            break
+        console.log('Erro')
+        break
+
 }
 
 
